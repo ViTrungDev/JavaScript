8abfbdd9ccdff7de7ec9a94911b7bdf3b45a67d7
Vi Trung <vitrung06022004@gmail.com>
2024-07-23 16:30:33 +0700

    phương thức nối chuỗi conCat, to uppercase. to lowercase , lấy ra vị trí index charAt

---
+++ js basic/chuỗi trong js/phương thức nối chuỗi concat/main.js
@@ -0,0 +1,15 @@
+// nối chuỗi concat
+let s1 = "hello";
+let s2 = "work";
+console.log(s1.concat(s2));
+// sử dụng viết hoa hết : toUppercase()
+let s3 = "hello wold";
+let s4 = s3.toUpperCase();
+console.log(s4);
+// bỏ viết hoa hết : toLowerCase()
+let s5 = "HELLO WOLD";
+let s6 = s5.toLowerCase();
+console.log(s6);
+// dùng charAt(index): để lấy giá trị tại vị trí
+let s7 = "HELLO WOLD";
+console.log(s7.charAt(2));
