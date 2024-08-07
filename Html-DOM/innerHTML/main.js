5db0971204e6d421cfe4ff0c4b45bf0ae127132a
Vi Trung <vitrung06022004@gmail.com>
2024-08-07 21:37:41 +0700

    InnerHTML

---
+++ Html-DOM/innerHTML/main.js
@@ -0,0 +1,6 @@
+var head = document.getElementsByClassName("heading").innerHTML;
+// heading.innerHTML = "Xem dom hah";
+console.log(head);
+
+let list = document.getElementsByClassName("list");
+list[0].innerHTML = "Học javaScript";
