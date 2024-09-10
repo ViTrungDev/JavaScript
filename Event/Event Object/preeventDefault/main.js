6d3454b36a25a6fefd10427c149fd178b13fed2e
Vi Trung <vitrung06022004@gmail.com>
2024-09-10 21:58:09 +0700

    Thực hành

--- Event/Event Object/preeventDefault/main.js
+++ Event/Event Object/preeventDefault/main.js
@@ -1,7 +1,16 @@
+// chọn phần tử ul cha
+const ul = document.getElementById("list");
+const nameUser = document.getElementById("name");
 // Ngăn chặn hành vi người dùng
 const login = document.getElementById("login");
 login.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log("form đã đc lưu");
   alert("form đã đc lưu");
+
+  console.log(nameUser.value);
+ 
+  const listUser = document.createElement("ul");
+  listUser.innerText = nameUser.value;
+  ul.appendChild(listUser);
 });
