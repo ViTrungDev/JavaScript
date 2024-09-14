986616831cbd49bd2cae93836a06632f635fe886
Vi Trung <vitrung06022004@gmail.com>
2024-09-14 23:20:37 +0700

    update function map in array js

---
+++ array/Map/main.js
@@ -0,0 +1,34 @@
+var course = [
+  {
+    id: 1,
+    name: "JavaScript",
+    coin: 0,
+  },
+  {
+    id: 2,
+    name: "Html/css",
+    coin: 2,
+  },
+  {
+    id: 3,
+    name: "Java",
+    coin: 3,
+  },
+  {
+    id: 4,
+    name: "C++",
+    coin: 4,
+  },
+];
+function CourseHandler(course) {
+  console.log(course);
+
+  return {
+    id: course.id,
+    name: `Khóa học: ${course.name}`,
+    coin: course.coin,
+    coinText: `Giá tiền: ${course.coin}`,
+  };
+}
+var newCourses = course.map(CourseHandler);
+console.log(newCourses);
